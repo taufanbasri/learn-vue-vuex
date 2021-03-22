714b0c1887d850e12bd31fbf8f9706d2748a0cf8
Taufan Abu Nada <taufanprasetyobasri@gmail.com>
2021-03-22 17:15:23 +0700

    Actions Async

--- src/main.js
+++ src/main.js
@@ -34,6 +34,16 @@ const store = createStore({
 
             return finalCounter;
         }
+    },
+    actions: {
+        increment(context) {
+            setTimeout(() => {
+                context.commit('increment');
+            }, 2000);
+        },
+        increase(context, payload) {
+            context.commit('increase', payload);
+        }
     }
 });
 
