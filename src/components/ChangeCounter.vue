ec40b617586c8682cad38fd43a0817ed63779495
Taufan Abu Nada <taufanprasetyobasri@gmail.com>
2021-03-22 19:59:17 +0700

    Map Helpers

--- src/components/ChangeCounter.vue
+++ src/components/ChangeCounter.vue
@@ -1,15 +1,14 @@
 <template>
-    <button @click="addOne">
-        Add 2
-    </button>
+    <button @click="increment">Add 1</button>
+    <button @click="increase({ value: 2 })">Add 2</button>
 </template>
 
 <script>
+import { mapActions } from "vuex";
+
 export default {
     methods: {
-        addOne() {
-            this.$store.dispatch('increment');
-        }
+        ...mapActions(['increment', 'increase'])
     },
 }
 </script>
